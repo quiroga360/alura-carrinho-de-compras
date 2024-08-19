536ecec4ee5695b40c5c31a0dc42045d67577523
Pablo - Note +A <pquiroga@maisaedu.com.br>
2024-08-19 08:14:36 -0300

    inicio

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const produtoInput = document.querySelector('#produto').value;
+const produtoQuantidade = document.querySelector('#quantidade').value;
+
+const carrinhoLista = document.querySelector('#lista-produtos');
+
+console.log(carrinhoLista);
